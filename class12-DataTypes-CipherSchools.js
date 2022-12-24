a6b1a4e97021e625afb8b8a0994170b5743a4ef5
alis-han <114800068+alis-han@users.noreply.github.com>
2022-12-24 14:48:10 +0530

    Add

---
+++ class12-DataTypes-CipherSchools.js
@@ -0,0 +1,21 @@
+// string
+let string="Text";
+console.log(string)
+
+// integer
+let integer=13;
+console.log(integer)
+
+// boolean
+let boolean=true;
+console.log(boolean)
+
+// null value
+let something=null;
+console.log(something)
+ 
+// undefined
+let a;
+console.log(a)
+
+// typeof keyword to know keyword type.
