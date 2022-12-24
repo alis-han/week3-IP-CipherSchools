b6b69c3198d59a9f97f87c13a801c3681161b916
alis-han <114800068+alis-han@users.noreply.github.com>
2022-12-24 10:51:53 +0530

    Add

---
+++ class7-modifyAttribute-CipherSchools.js
@@ -0,0 +1,8 @@
+let attribute=document.querySelector("img").attributes;
+console.log(attribute);
+
+let hasattribute=document.querySelector("img").hasAttribute();
+console.log(hasattribute)
+
+let getattribute=document.querySelector("img").getAttribute();
+console.log(getattribute)
