0e44e6ddf39b069990425ce90e5ff031e56301fc
alis-han <114800068+alis-han@users.noreply.github.com>
2022-12-24 15:29:09 +0530

    Add

---
+++ class20-DisplayinBrowser-CipherSchools.js
@@ -0,0 +1,10 @@
+const content=`
+    <main>
+        <ul>
+            <li>Name:${newPencilbox.name}</li>
+            <li>Name:${newPencilbox.size}</li>
+            <li>Name:${newPencilbox.namee}</li>
+        </ul>
+    </main>
+
+`;
